78124d72fab4a6132f103033a11d005f3cb89ceb
Wennya Rabelo <wennyarabelo@gmail.com>
2023-08-24 10:49:48 -0300

    Implementação da funcionalidade para capturar o username do usuário digitado no input.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,40 @@
 import Image from "next/image"
 import * as S from "./page.styles"
 import { InputSearch } from "@/components/InputSearch"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const [value, setValue] = useState<string>("");
+
+  const onClickSearch = () => {
+    if (value) {
+      router.push(`/profile/${value}`)
+    }
+  }
+
   return (
     <S.Container>
-      <Image 
-      src="/images/logo-home.svg"
-      alt="Logotipo search devs"
-      width={478}
-      height={64}
-      className="logo"
+      <Image
+        src="/images/logo-home.svg"
+        alt="Logotipo search devs"
+        width={478}
+        height={64}
+        className="logo"
       />
 
       <S.ContainerSearch>
-        <InputSearch />
-        <button className="btn-search">Search</button>
+        <InputSearch
+          onChange={(value) => { setValue(value) }}
+        />
+
+        <button
+          className="btn-search"
+          onClick={() => onClickSearch()}
+        >Search</button>
       </S.ContainerSearch>
     </S.Container>
   )
--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -1,16 +1,34 @@
 import { Search } from "@mui/icons-material"
 import * as S from "./styles"
+import { ChangeEvent, useEffect, useState } from "react"
 
-export function InputSearch() {
+interface IInputSearch {
+  onChange(value: string): void
+}
+
+export function InputSearch({ onChange }: IInputSearch) {
+
+  const [value, setValue] = useState<string>("");
+
+  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setValue(value)
+  }
+
+  useEffect(() => {
+    onChange(value)
+  }, [value])
 
   return (
     <S.Container>
-      <Search 
+      <Search
         className="iconSearch"
       />
       <input
         placeholder="Search"
         className="inputSearch"
+        value={value}
+        onChange={(e) => onChangeInput(e)}
       />
     </S.Container>
   )
